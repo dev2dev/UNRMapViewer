a182bc30428458e4cca71114110eb03b127e2a48
adudney <andrew.dudney@gmail.com>
2011-07-15 10:52:15 -0600

    Fixed class loading plugin, (and function, and state, and struct) changed back to 32bit rendering, fixed class obj reference resolving, added guid and unreal-script loading, added default properties loading to UNRMap, fixed lightmap creation

---
+++ UNRMapViewer/UnrealPackage/UNRPluginObject/UNRScriptParser.h
@@ -0,0 +1,11 @@
+//
+//  UNRScriptParser.h
+//  UnrealPackageExporter
+//
+//  Created by Andrew Dudney on 7/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+@class UNRObject;
+
+NSMutableData *loadScript(UNRObject *self, int size);
